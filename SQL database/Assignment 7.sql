110d3a9b341074da64aa811aaebe3dd7ae9ff493
MohamedYehia2219 <77175242+MohamedYehia2219@users.noreply.github.com>
2024-06-05 23:35:32 +0300

    session 7

--- SQL database/Assignment 7.sql
+++ SQL database/Assignment 7.sql
@@ -15,8 +15,20 @@ from Instructor
 order by Salary desc
 
 --Write a query to select the highest two salaries in Each Department for instructors who have salaries. “using one of Ranking Functions”
+select * 
+from
+(select salary, Dept_Id, ROW_NUMBER() over (partition by Dept_Id order by salary desc) as 'rank'
+from Instructor 
+where Salary is not null) as newTable
+where rank in (1,2)
 
 --Write a query to select a random  student from each department.  “using one of Ranking Functions”
+select *
+from
+(select St_Fname, Dept_Id , ROW_NUMBER() over (partition by Dept_Id order by newID()) as 'rank'
+from Student 
+where Dept_Id is not null) as newTable
+where rank = 1
 -----------------------------------------------------------------------------------------
 use MyCompany
 
@@ -25,4 +37,28 @@ select d.*
 from Departments d, Employee e
 where d.Dnum = e.Dno and e.SSN = (select min(SSN) from Employee)
 
---
+--List the last name of all managers who have no dependents.
+select lname
+from Employee
+where SSN not in (select distinct essn from Dependent)
+
+--For each department if its average salary is less than the average salary of all employees display its number,
+--name and number of its employees.
+select e.Dno, d.Dname, count(*) as 'employeess count'
+from Employee e, Departments d
+where d.Dnum = e.Dno 
+group by e.dno, d.Dname
+having avg(Salary)  < (select avg(Salary)from Employee)
+						
+--Try to get the max 2 salaries using subquery
+select top(2) *
+from (select Salary, ROW_NUMBER() over(order by salary desc) as 'rank'
+		from Employee) as newTable
+
+--Display the employee number and name if he/she has at least one dependent (use exists keyword) self-study
+select e.SSN ,e.Fname from Employee e
+where exists
+( select d.essn 
+from Dependent d
+where e.SSN = d.ESSN )
+	
