294809ab20ca8d7123fb3a10c89f0ae1222b7a1e
MohamedYehia2219 <77175242+MohamedYehia2219@users.noreply.github.com>
2024-06-30 01:33:47 +0300

    session2 c#

---
+++ C#/Session2 solution/Assignment02/Program.cs
@@ -0,0 +1,134 @@
+﻿namespace Assignment02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            //Console.WriteLine(Console.ReadLine());    //as string
+            //Console.WriteLine("Enter a Number");
+            //float Number = float.Parse(Console.ReadLine());
+            //Console.WriteLine(Number); 
+            #endregion
+
+            #region Q2
+            //If there are characters, that will lead to an Exception 
+            //int Number = int.Parse(Console.ReadLine());
+            //Console.WriteLine(Number);
+
+            //using TryParse, if there are characters, the int number will = 0 
+            //int Number2;
+            //int.TryParse(Console.ReadLine(),out Number2);
+            //Console.WriteLine(Number2);
+            #endregion
+
+            #region Q3
+            //float Number1 = 5.5f;
+            //float Number2 = 4.5f;
+            //float sum = Number1 + Number2;
+            //float difference = Number1 - Number2;
+            //Console.WriteLine("Sum: " + sum);
+            //Console.WriteLine("Difference: " + difference);
+            #endregion
+
+            #region Q4
+            //Console.WriteLine("Enter string");
+            //string Input = Console.ReadLine();
+            //Console.WriteLine(Input.Substring(0,1));
+            #endregion
+
+            #region Q5
+            //int x = 5;
+            //int y = x;
+            //Console.WriteLine("X = " + x + "\t" + "Y = "+ y);
+            //x = 7;
+            //Console.WriteLine("X = " + x + "\t" + "Y = " + y);
+            #endregion
+
+            #region Q6
+            //point p1 = new point();
+            //p1.x = 5;
+            //point p2 = new point();
+            //p2 = p1;
+            //Console.WriteLine(p2.x);
+            //p2.x = 7;
+            //Console.WriteLine(p1.x);
+            #endregion
+
+            #region Q7
+            //Console.WriteLine("Enter string 1");
+            //string s1 = Console.ReadLine();
+            //Console.WriteLine("Enter string 2");
+            //string s2 = Console.ReadLine();
+            //string s = s1 + " " + s2;
+            //Console.WriteLine(s);
+            #endregion
+
+            #region Q8
+            //Console.WriteLine("Enter principal");
+            //float Principal = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter rate");
+            //float Rate = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter time");
+            //float Time = float.Parse(Console.ReadLine());
+            //float Interest = (Principal * Rate * Time) / 100;
+            //Console.WriteLine("Interest = " + Interest);
+            #endregion
+
+            #region Q9
+            //Console.WriteLine("Enter Weight");
+            //float Weight = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Height");
+            //float Height = float.Parse(Console.ReadLine());
+            //float BMI = Weight / (Height * Height);
+            //Console.WriteLine("BMI = " + BMI);
+            #endregion
+
+            #region Q10
+            //Console.WriteLine("Enter Temperature");
+            //float Temperature = float.Parse(Console.ReadLine());
+            //string result;
+            //result = Temperature < 10 ? "Cold" : (Temperature > 30 ? "Hot" : "Good");
+            //Console.WriteLine(result);
+            #endregion
+
+            #region Q11
+            //Console.WriteLine("Enter Day");
+            //string day = Console.ReadLine();
+            //Console.WriteLine("Enter month");
+            //string month = Console.ReadLine();
+            //Console.WriteLine("Enter year");
+            //string year = Console.ReadLine();
+            //Console.WriteLine($"Today’s date : {day}/{month}/{year}");
+            //Console.WriteLine($"Today’s date : {day},{month},{year}");
+            //Console.WriteLine($"Today’s date : {day}-{month}-{year}");
+            #endregion
+
+            #region Q12
+            //DateTime date = new DateTime(2024, 6, 14); 
+            //Console.WriteLine($"The event is on {date:MM/dd/yyyy}");
+            //The event is on 06/14/2024
+            #endregion
+
+            #region Q13
+            //int d;
+            //d = Convert.ToInt32(!(30 < 20));
+            //Console.WriteLine(d);   // d=1
+            //int x = Convert.ToInt32(true);
+            //Console.WriteLine(x);
+            #endregion
+
+            #region Q14
+            //Console.WriteLine(13 / 2 + " " + 13 % 2);   //  6  1
+            #endregion
+
+            #region Q15
+            //int num = 1, z = 5;
+            //if (!(num <= 0))
+            //    Console.WriteLine(++num + z++ + " " + ++z);     //7 7 
+            //else
+            //    Console.WriteLine(--num + z-- + " " + --z);
+            #endregion
+        }
+    }
+}
---
+++ C#/Session2 solution/Session2/Program.cs
@@ -0,0 +1,199 @@
+﻿using System.Net.Sockets;
+
+namespace Session2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region revision
+            // comment --> ctrl + k + c
+            // region --> ctrl + k + s
+            //Console.WriteLine("Hello, World!");
+            //Console.ReadKey(); 
+            #endregion
+
+            #region Variable declaration
+            //Variable Declaration
+            // variable: value container in memory
+            // data tybe, name (pascal case), value, address(hexa decimal) 
+            // variable name can't be a reserved word (class, namespace, int,...) 
+            // naming convention:
+            //PascalCase --> FirstNmae
+            //camelCase --> firstName
+            //kabab_case--> first_name
+            /*int Number;         //declaration
+            Number = 10;        // initialization
+            Console.WriteLine(Number);
+            int Number1 = 10, Number2 = 20;
+            Console.WriteLine(Number1 + Number2);*/
+            #endregion
+
+            #region Value Type (primitive)
+            //Data types (value type - primitive)
+            //int Number1;        //c# keyword
+            //Int32 Number2 = 10;
+            //Number1 = Number2;
+            //Console.WriteLine("Number1 = " + Number1);
+            //Console.WriteLine("Number2 = " +  Number2); 
+            #endregion
+
+            #region Refrence Type (Non - primitive)
+            /*Point p1;
+            // allocate refrence in the Stack
+            // 8 bytes have been allocated in Stack
+            // 0 bytes have been allocated in Heap
+            p1 = new Point();
+            // new: 1- allocte the required bytes for the obj in the Heap --> ( 8 + CLR overhead bytes )
+            //      2- intialize bytes in Heap with the defaulat value
+            //      3- call the user defined constructor if exists
+            //      4- assign the allocated obj in Heap to the reference in the Stack
+            Point p2 = new Point();
+            p2 = p1;    //  p2 referes to p1
+            p2.y = 5;
+            Console.WriteLine(p1.y);
+            Console.WriteLine(p2.y);
+            Console.WriteLine(p1.x);*/
+            #endregion
+
+            #region Object
+            //Point p1 = new Point();
+            //Console.WriteLine(p1);              //name space 
+            //Console.WriteLine(p1.ToString());   //name space
+
+            //object o1 = new object();
+            //Console.WriteLine(o1);
+            //o1 = new String("mohamed");
+            //Console.WriteLine(o1);
+            //o1 = "yehia";
+            //o1 = 3.14;
+            //o1 = true;
+
+            //object o2 = new object();   
+            //object o3 = new object();
+            //Console.WriteLine(o2.GetHashCode() +"\t" + o3.GetHashCode());
+            //o2 = o3;
+            //Console.WriteLine(o2.GetHashCode() + "\t" + o3.GetHashCode());
+            #endregion
+
+            #region Casting
+            // ********** implicit (safe casting)
+            //int x = 4;
+            //double y = x;
+            //long z = x;
+
+            // *************** Explicit (unsafe casting)
+            //int x1 = 5;
+            //long y1 = 321;
+            //x1 = (int) y1;
+            //Console.WriteLine(x1);
+
+            //int x2;
+            //long y2 = 321456789741285963;
+            //x2 = (int) y2;
+            //Console.WriteLine(x2);  // random number
+
+            //int x3 = 5;
+            //long y3;
+            //checked
+            //{
+            //    unchecked
+            //    {
+            //        y3 = 321456789741285963;
+            //    }
+            //    x3 = (int) y3;   // System.OverflowException: Exception
+            //}
+            //Console.WriteLine(x3);
+            #endregion
+
+            #region Parsing
+            //int x = int.Parse("123");
+            //Console.WriteLine(x+1);
+
+            //Console.WriteLine("Enter age");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter GPA");
+            //double gpa = double.Parse(Console.ReadLine());
+            //Console.WriteLine("age: " + age + "\t" + "GPA: " + gpa);
+
+            //Console.WriteLine("Enter age");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter GPA");
+            //double gpa = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("age: " + age + "\t" + "GPA: " + gpa);
+
+            //int age;
+            //bool flag = int.TryParse(Console.ReadLine(), out age);
+            //Console.WriteLine(age + "\t" + flag);
+            #endregion
+
+            #region Fractions
+            //fractions
+            //2.5 --> double
+            //double x = 2.5;
+            //float y1 = 2.5f;
+            //float y2 = (float)2.5;
+            //decimal z1 = 2.5m;
+            //decimal z2 = (decimal)2.5;
+
+            //// Discard (readability)
+            //long a = 100_000_000;
+            //Console.WriteLine(a);
+            #endregion
+
+            #region operators
+            //1- unary operators --> works on one variable ++, --
+            /* int x = 10;
+            // prefix [increment then print]
+            Console.WriteLine(++x);                 //11
+            //postfix [print then increment]
+            Console.WriteLine(x++);                //11
+            Console.WriteLine(x); */                //12
+
+            //2- binary operators (+,-,*,/,%)
+            //int Number1 = 2, Number2 = 6;
+            //Console.WriteLine(Number1%Number2);
+
+            //3- Assignment operators
+            //int x = 4;
+            //x += 2;
+            //x -= 2;
+            //x /= 2;
+            //x *= 2;
+            //x %= 2;     // x = x%2
+
+            //4- Relational operators (comparison)
+            //int x= 5; int y= 10;
+            //Console.WriteLine(x == 10);
+            //Console.WriteLine(x == y);
+            //Console.WriteLine(x >= y);
+            //Console.WriteLine(x > y);
+            //Console.WriteLine(x < y);
+            //Console.WriteLine(x <= y);
+            //Console.WriteLine(x != y);
+
+            //5- Logical operators [!, ||, &&] (AND, OR short circles)
+            //Console.WriteLine(!false);
+            //Console.WriteLine(false || true);
+            //Console.WriteLine(false && true);
+
+            //6- Logical operators [!, |, &] (AND, OR long circles)
+            //Console.WriteLine(!false);
+            //Console.WriteLine(false | true);
+            //Console.WriteLine(false & true);
+
+            //7- Ternary operator [conditional]
+            //bool flag = 4 > 2;
+            //Console.WriteLine(flag);
+            //string flag2 = 4 > 2 ? "correct" : "not correct";
+            //Console.WriteLine(flag2);
+
+            //operators priority [precedence]
+            // 1- unary operators
+            // 2- round braces ()
+            // 3- *,/
+            // 4- +,-
+            #endregion
+        }
+    }
+}
